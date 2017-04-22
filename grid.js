e82024c0b07931fc3b8b7ac2d55e9ad8afd45640
Constance Ye <constanceye1316@gmail.com>
2017-04-22 02:46:54 -0700

    added obstacles
    built it so changes in width/height dont remove the obstacles

--- grid.js
+++ grid.js
@@ -1,7 +1,8 @@
 var blockWid = 50;
 var width = 5;
 var height = 5;
-// var obstacles = [new Cell(0, 0, blockWid, true, [0, 0])];
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 
 // Grid class
 function Grid() {
@@ -11,38 +12,57 @@ function Grid() {
     for (var i = 0; i < this.wid; i++) {
         var row = [];
         for (var j = 0; j < this.hei; j++) {
-            var temparr = [i, j];
-            row.push(new Cell(i * blockWid, j * blockWid, blockWid, false, temparr));
+            row.push(new Cell(i * blockWid, j * blockWid, blockWid, i, j));
         }
         this.grid.push(row);
     }
 }
 
 //Cell class
-function Cell(xcoor, ycoor, wid, tf, arrayPos) {
+function Cell(xcoor, ycoor, wid, a, b) {
     this.width = wid;
     this.x = xcoor;
     this.y = ycoor;
-    this.obstacle = tf;
-    this.arrayPos = arrayPos;
+    this.arrayPos = [a, b];
+    this.bot = false;
+    this.obstacle = false;
 }
 
 function drawGrid(m) {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
     c.width = width * blockWid;
     c.height = height * blockWid;
     for (var i = 0; i < m.grid.length; i++) {
         for (var j = 0; j < m.grid[0].length; j++) {
             var temp = m.grid[i][j];
+            if(temp.obstacle){
+                ctx.fillRect(temp.x, temp.y, temp.width, temp.width);
+            }
             ctx.rect(temp.x, temp.y, temp.width, temp.width);
             ctx.stroke();
         }
     }
-    // for (var i = 0; i < obstacles.length; i++) {
-    //     var temp = obstacles[i];
-    //     ctx.fillRect(temp.x, temp.y, temp.width, temp.width);
-    // }
+}
+function returnObs(m){
+    var arr = [];
+    for (var i = 0; i < m.grid.length; i++) {
+        for (var j = 0; j < m.grid[0].length; j++) {
+            var temp = m.grid[i][j];
+            if(temp.obstacle){
+                arr.push(temp);
+            }
+        }
+    }
+    return arr;
+}
+function setPrevObstacles(m, arr){
+    for (var i = 0; i < arr.length; i++) {
+        var temp = arr[i];
+        if(temp.arrayPos[0] < m.length && temp.arrayPos[1] < m[0].length){
+            var cell = m[temp.arrayPos[0]][temp.arrayPos[1]];
+            cell.obstacle = true;
+        }
+        
+    }
 }
 
 var adjust = function() {
@@ -52,17 +72,23 @@ var adjust = function() {
     if (this.id === "heightslider") {
         height = this.value;
     }
-
-    n = new Grid();
-    drawGrid(n);
+    var obstacles = returnObs(m);
+    m = new Grid();
+    setPrevObstacles(m.grid, obstacles);
+    drawGrid(m);
 };
 
 document.getElementById("widthslider").addEventListener("mouseup", adjust);
 document.getElementById("heightslider").addEventListener("mouseup", adjust);
 
-// document.getElementById("myCanvas").addEventListener("click", function() {
+document.getElementById("myCanvas").addEventListener("click", function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var temp = m.grid[(x - x%blockWid)/blockWid][(y - y%blockWid)/blockWid];
+    temp.obstacle = true;
+    ctx.fillRect(temp.x, temp.y, temp.width, temp.width);
 
-// });
+});
 
 var m = new Grid();
 drawGrid(m);
